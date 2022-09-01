ba7a1e0453c08f9b5a1161bd595aeeb5f56cde9d
Mark Logan <mark@marklgn.com>
2022-08-31 19:09:55 -0700

    Remove tonic

--- madsim-tonic/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(madsim)]
-#[path = "sim.rs"]
-mod sim;
-
-#[cfg(madsim)]
-pub use sim::*;
-#[cfg(not(madsim))]
-pub use tonic::*;
--- tonic-example/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=proto/helloworld.proto");
-    tonic_build::compile_protos("proto/helloworld.proto")?;
-    Ok(())
-}
